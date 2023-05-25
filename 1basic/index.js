6c70cb186489761071553490718d2c44d665fff8
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-05-25 17:43:59 +0530

    Add files via upload

---
+++ 1basic/index.js
@@ -0,0 +1,37 @@
+//console.log("hello");
+// var a = 20;
+// var b = 30;
+// let c = a + b;
+// console.log(c);
+
+// var x = 20;
+// if (x == 20) {
+//   console.log("matched");
+// } else {
+//   alert("Not matched");
+// }
+
+// var x = '20';
+// for(int i=0;i<navigator;i++){
+//   console.log(i);
+// }
+
+// const arr = [2, 4, 7, 1, 3, 8, 3];
+// console.log(arr);
+
+//filter
+// const arr = [2, 4, 7, 1, 3, 8, 3];
+// let result = arr.filter(item => {
+//   //console.log(item);
+//   //return item == 3;
+//   return item >= 4;
+// });
+// console.log(result);
+
+//Color package use
+// const colors = require('colors');
+// console.log("Hello".red)
+// console.log("Hello".bgBlue)
+
+//console.log("Node js");
+//console.log(100 + 20);
