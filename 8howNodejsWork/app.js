9b7a8714d9213ff39a242532c23581c76d5f0356
Bittu Kumar <97692048+Bittu121@users.noreply.github.com>
2023-05-25 17:48:46 +0530

    Add files via upload

---
+++ 8howNodejsWork/app.js
@@ -0,0 +1,18 @@
+
+//How Nodejs works
+//1.call stack
+//2.Node API
+//3.callBackQueue
+//4.Example
+//5.interview Questions
+
+// Note:-Architecture-> 1.callstack 2.NodeApi 3.callBacl Queue
+// console.log("Starting Up");
+// setTimeout(() => {
+//   console.log("2 sec log");
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log("0 second log");
+// }, 0);
+// console.log("finishing Up");
